ff3e1cd7766ccd00b543b09989e29571c11775f0
JGVR <juangabrielvasquez11@gmail.com>
2024-12-23 00:49:02 -0500

    added more styling to the navbar. Current Link Background changes color to a different blue when client is in current page

--- portfolio-hub/components/NavBar.tsx
+++ portfolio-hub/components/NavBar.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import Link from "next/link"
 import Image from "next/image"
 import { UserIcon, BriefcaseIcon, FolderIcon } from "@heroicons/react/16/solid"
+import { usePathname } from "next/navigation";
 
 export default function NavBar(){
+    const pathName = usePathname();
+
     return(
         <nav className="grid grid-rows-[auto,1fr] h-screen bg-prussian-blue">
             <div className="flex flex-col items-center w-72">
@@ -14,19 +19,25 @@ export default function NavBar(){
                 <h1 className="text-2xl font-sans font-bold italic mt-2">Juan Vasquez</h1>
                 <h3 className="font-serif text-sm">Software Developer</h3>
             </div>
-            <div className="flex flex-col items-start w-72 pt-12 pl-5 pr-3">
-                <div className="flex flex-row w-full h-10 items-center">
-                    <UserIcon className="h-6 w-6 mr-3 text-white"/>
-                    <Link href="/profile" className="text-lg font-serif">Profile</Link>
-                </div>
-                <div className="flex flex-row w-full h-10 items-center mt-2">
-                    <BriefcaseIcon className="h-6 w-6 mr-3 text-white"/>
-                    <Link href="/projects" className="text-lg font-serif">Experience</Link>
-                </div>
-                <div className="flex flex-row w-full h-10 items-center mt-2">
-                    <FolderIcon className="h-6 w-6 mr-3 text-white"/>
-                    <Link href="/experience" className="text-lg font-serif">Projects</Link>
-                </div>
+            <div className="flex flex-col items-start w-72 pt-12 pl-5 pr-5">
+                <Link href="/" className="text-lg font-serif w-full">
+                    <div className={`flex flex-row w-full h-10 items-center p-2 rounded-lg hover:bg-lapis-lazuli ${pathName === "/" ? "bg-lapis-lazuli" : ""}`}>
+                        <UserIcon className="h-6 w-6 mr-3 text-white"/>
+                        <h3>Profile</h3>
+                    </div>
+                </Link>
+                <Link href="/experience" className="text-lg font-serif w-full">
+                    <div className={`flex flex-row w-full h-10 items-center mt-2 p-2 rounded-lg hover:bg-lapis-lazuli ${pathName === "/experience" ? "bg-lapis-lazuli" : ""}`}>
+                        <BriefcaseIcon className="h-6 w-6 mr-3 text-white"/>
+                        <h3>Experience</h3>
+                    </div>
+                </Link>
+                <Link href="/projects" className="text-lg font-serif w-full">
+                    <div className={`flex flex-row w-full h-10 items-center mt-2 p-2 rounded-lg hover:bg-lapis-lazuli ${pathName === "/projects" ? "bg-lapis-lazuli" : ""}`}>
+                        <FolderIcon className="h-6 w-6 mr-3 text-white"/>
+                        <h3>Projects</h3>
+                    </div>
+                </Link>
             </div>
         </nav>
     )
