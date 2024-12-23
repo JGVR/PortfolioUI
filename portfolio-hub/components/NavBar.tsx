8e9cafd21c46238be964af8869ea9dcf7b0c1812
JGVR <juangabrielvasquez11@gmail.com>
2024-12-22 22:51:46 -0500

    completed nav bar. Added icons with Heroicon to each link. Also added custom spacing to the nav bar grid

--- portfolio-hub/components/NavBar.tsx
+++ portfolio-hub/components/NavBar.tsx
@@ -1,10 +1,11 @@
 import Link from "next/link"
 import Image from "next/image"
+import { UserIcon, BriefcaseIcon, FolderIcon } from "@heroicons/react/16/solid"
 
 export default function NavBar(){
     return(
-        <nav className="grid grid-cols-1 h-screen bg-prussian-blue">
-            <div className="flex flex-col items-center w-72 rounded-lg">
+        <nav className="grid grid-rows-[auto,1fr] h-screen bg-prussian-blue">
+            <div className="flex flex-col items-center w-72">
                 <Image className="mt-5 border rounded-full"
                         src="https://stbc.blob.core.windows.net/portfolio-hub/JuanVasquez-Pic.jpeg"
                         alt="Juan Vasquez"
@@ -12,9 +13,20 @@ export default function NavBar(){
                         height={200}/>
                 <h1 className="text-2xl font-sans font-bold italic mt-2">Juan Vasquez</h1>
                 <h3 className="font-serif text-sm">Software Developer</h3>
-                <Link href="/profile" className="text-lg font-serif mt-10">Profile</Link>
-                <Link href="/projects" className="text-lg font-serif mt-5">Experience</Link>
-                <Link href="/experience" className="text-lg font-serif mt-5">Projects</Link>
+            </div>
+            <div className="flex flex-col items-start w-72 pt-12 pl-5 pr-3">
+                <div className="flex flex-row w-full h-10 items-center">
+                    <UserIcon className="h-6 w-6 mr-3 text-white"/>
+                    <Link href="/profile" className="text-lg font-serif">Profile</Link>
+                </div>
+                <div className="flex flex-row w-full h-10 items-center mt-2">
+                    <BriefcaseIcon className="h-6 w-6 mr-3 text-white"/>
+                    <Link href="/projects" className="text-lg font-serif">Experience</Link>
+                </div>
+                <div className="flex flex-row w-full h-10 items-center mt-2">
+                    <FolderIcon className="h-6 w-6 mr-3 text-white"/>
+                    <Link href="/experience" className="text-lg font-serif">Projects</Link>
+                </div>
             </div>
         </nav>
     )
