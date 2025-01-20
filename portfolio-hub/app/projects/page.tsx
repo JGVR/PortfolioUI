1d91628d134aee1c0cc1e2d6a181393989acd42f
JGVR <juangabrielvasquez11@gmail.com>
2025-01-19 23:42:27 -0500

    fixed reference to ChatBotModal in the projects page

--- portfolio-hub/app/projects/page.tsx
+++ portfolio-hub/app/projects/page.tsx
@@ -1,11 +1,8 @@
 'use client';
 import ChatBotModal from "@/components/ChatBotModal";
-import Chatbot from "@/components/Chatbot";
 
 export default function Projects(){
     return(
-        <ChatBotModal>
-            <Chatbot/>
-        </ChatBotModal>
+        <ChatBotModal/>
     )
 }
